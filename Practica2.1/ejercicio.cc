e49e773a8e8c2bbe97de6025c3aba6f21a84bd27
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-07 10:23:31 +0100

    Update ejercicio.cc

--- Practica2.1/ejercicio.cc
+++ Practica2.1/ejercicio.cc
@@ -12,3 +12,13 @@ int main() {
       printf("Correct.");
    return 1;
 }
+
+/*
+Comando: gcc -o ejer1 ejer1.c
+         ./ejer1
+
+Resultado
+Error: : Operation not permitted
+porque no existe el usuario 2, y setuid(2) = -1
+
+*/
