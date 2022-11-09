b37b9c13652beb794f39e96b4eae316ac44a0529
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-09 22:34:50 +0100

    Create ejercicio7.c

---
+++ Practica2.1/ejercicio7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/utsname.h>
+#include <stdlib.h>
+
+int main() {
+   //long fpathconf(int fd, int name);
+   //long pathconf(char *path, int name);
+  char *path = "/D";
+  printf("The maximum number of links to the file: %li\n", pathconf(path, _PC_LINK_MAX));
+  printf("The maximum length of a relative pathname when path or fd is the current working directory: %li\n", pathconf(path, _PC_PATH_MAX));
+  printf("The maximum length of a filename in the directory path or fd that the process is allowed to create: %li\n", pathconf(path, _PC_NAME_MAX));
+  
+  return 0;
+}
+
