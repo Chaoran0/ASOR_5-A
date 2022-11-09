c596ed1657c56af213f241acdb7778d318817e69
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-09 22:57:50 +0100

    Update ejercicio6.c

--- Practica2.1/ejercicio6.c
+++ Practica2.1/ejercicio6.c
@@ -10,9 +10,9 @@
 int main() {
    //long sysconf(int name);
 
-  printf("The maximum length of the arguments: %li\n", sysconf(_SC_ARG_MAX));
-  printf("The maximum number of simultaneous processes per user ID: %li\n", sysconf(_SC_CHILD_MAX));
-  printf("The maximum number of files that a process can have open at any time: %li\n", sysconf(_SC_OPEN_MAX));
+  printf("The maximum length of the arguments: %ld\n", sysconf(_SC_ARG_MAX));
+  printf("The maximum number of simultaneous processes per user ID: %ld\n", sysconf(_SC_CHILD_MAX));
+  printf("The maximum number of files that a process can have open at any time: %ld\n", sysconf(_SC_OPEN_MAX));
  
   return 0; 
 }
