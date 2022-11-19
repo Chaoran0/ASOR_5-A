97aa048e75bd6a486f790259e0114939bc537ee7
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-19 22:57:22 +0100

    Create ejercicio5.c

---
+++ Pratica2.2/ejercicio5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+
+  const char PATH[]="/home/Chaoran/ASOR/PR1/GE/ej5.txt";
+
+  int df = open(PATH, O_CREAT, 0645);
+
+  printf("Descriptor = %i\n", df);
+
+
+  return 1;
+}
