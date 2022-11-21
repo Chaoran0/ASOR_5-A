1ea4d030190b49629b9397d5ff1f058b401dbb40
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-21 11:07:56 +0100

    Update ejercicioXI.c

--- Pratica2.2/ejercicioXI.c
+++ Pratica2.2/ejercicioXI.c
@@ -13,12 +13,23 @@ int main(int argc, char *argv[]){
     
     fd = open(argv[1], O_CREAT|O_WRONLY|O_WRONLY, 0777);
 
-    const char fd1 = fd;
-    const char x1 = "prob11.txt";
-    //const char x2 = "prob11s.txt";
-    link(&fd1, &x1);
-    //symlink(&fd1, &x2);
+    link(argv[1], "x.txt");
+    symlink(argv[1], "y.txt");
 
 
     return 0;
 }
+
+/*
+usuario_vms@portatil:~/Descargas$ gcc -o ejer ejer.c
+usuario_vms@portatil:~/Descargas$ ./ejer 1.txt
+usuario_vms@portatil:~/Descargas$ ls -l
+total 31284
+-rw-r--r-- 3 usuario_vms users       17 nov 21 12:09 1.txt
+-rwxr-xr-x 1 usuario_vms users    16104 nov 21 12:09 ejer
+-rw-r--r-- 1 usuario_vms users      424 nov 21 12:09 ejer.c
+-rw-r--r-- 3 usuario_vms users       17 nov 21 12:09 x.txt
+lrwxrwxrwx 1 usuario_vms users        5 nov 21 12:10 y.txt -> 1.txt
+
+x.txt e y.txt no existían
+*/
