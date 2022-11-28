1a423a58a8a8dc6ef177ae44820e5064e9d85dfc
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-28 18:03:40 +0100

    Create ejercicio8.c

---
+++ Practica2.3/ejercicio8.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main (int argc, char *argv[]){
+    //int execve(const char *filename, char *const argv[], char *const envp[]);
+    if (argc < 2) {
+        printf("ERROR: Introduce el comando.\n");
+        exit(1);
+    }
+
+    pid_t pid = fork();
+    switch(pid){
+        case -1:
+            perror("Error");
+            exit(1);
+        case 0:
+            setsid();
+            chdir("/tmp");
+            printf("Hijo %i (padre: %i)\n", getpid(), getppid());
+            int fd = open("/tmp/daemon.out",O_CREAT | O_RDWR, 0777);
+            int err = open("/tmp/daemon.err",O_CREAT | O_RDWR, 0777);
+            int dn = open("/dev/null",O_CREAT | O_RDWR, 0777);
+            /*tanto execl como execv ignoran la variable PATH, que contiene las rutas de búsqueda. 
+            Para tener en cuenta esta variable pueden usarse las versiones execlp o execvp. */
+            //char *newargv[] = {argv[1], NULL};
+            dup2(fd, 2);
+            dup2(err, 1);
+            dup2(dn, 0);
+            if (execvp(argv[1], argv+1) == -1) {
+                printf("ERROR execvp\n");
+                exit(-1);
+            }
+
+            
+        break;
+        default:
+            printf("Padre %i (hijo: %i)\n", getpid(), pid);
+        break;
+    }
+    return 0;
+}
