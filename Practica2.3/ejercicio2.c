e4f392352d11b264ca85123c8447499deb80aa69
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-28 09:31:46 +0100

    Update ejercicio2.c

--- Practica2.3/ejercicio2.c
+++ Practica2.3/ejercicio2.c
@@ -1,1 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+int main (int argc, char *argv[]){
+    //int nice(int inc);
+    
+    if (argc != 2){
+        printf("Argument failed\n");
+        exit(1);
+    }
+    int sol = nice(argv[1]);
+    printf("El valor de nice es: %d\n", sol);
+
+    return 0;
+}
