8158c3a97866e6210151b90c57abb09304047cf6
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-29 15:47:44 +0100

    Update ejercicioXIII.c

--- Practica2.3/ejercicioXIII.c
+++ Practica2.3/ejercicioXIII.c
@@ -13,32 +13,32 @@ void sighandler (int signo){
 int main(int argc, char *argv[]) {
   int secs = atoi(argv[1]);
   if (argc != 2) {
-    printf("ERROR: Introduce los segundos!\n");
+    printf("ERROR: incorrect arguments\n");
     exit(1);
   }
 
   struct sigaction act;
   //Sigint
   sigaction(SIGUSR1, NULL, &act); //Get handler
-  act.sa_handler = &sighandler;
+  act.sa_handler = sighandler;
   sigaction(SIGUSR1, &act, NULL); //Set sa_handler
 
-  printf("Se va a borrar el fichero %s después de %d segundos. Envía SIGSR1 para detener.\n", argv[0], secs);
+  printf("Se va a borrar el fichero %s después de %d segundos. Envía SIGUSR1 para detener.\n", argv[0], secs);
 
   int i = 0;
-  
+  alarm(secs+1);
 	while (i < secs && stop == 0) {
     i++;
     sleep(1);
-    alarm(secs-i);
+    
   }
   
 
   if (stop == 0) {
-    printf("Se va a borrar");
+    printf("Borrado\n");
     unlink(argv[0]);
   } else {
-    printf("Has tenido suerte!");
+    printf("Impedido\n");
   }
     
   return 0;
