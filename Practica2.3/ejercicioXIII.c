2709e5fe3215421200494b4d45dcda77c5d5eb74
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-11-29 14:20:01 +0100

    Create ejercicioXIII.c

---
+++ Practica2.3/ejercicioXIII.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+volatile int 
+
+void sighandler (int signo){
+    
+}
+
+int main(int argc, char *argv[]) {
+  int secs = atoi(argv[1]);
+
+    
+  return 0;
+}
