6d59f1cc7b2595c5bdd94f9592d895e4c481bfca
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-12-04 18:01:49 +0100

    Tubería hecha

--- Practica2.4/ejercicio1.c
+++ Practica2.4/ejercicio1.c
@@ -7,10 +7,35 @@ int main (int argc, char *argv[]){
 
     if(argc < 5){
         perror("Error: Argumento insuficiente. \n");
+        exit(0);
+    }
+    
+    int fd[2];
+    if(pipe(fd) == -1){
+        perror("Error: Función pipe fallida. \n");
         exit(-1);
     }
 
-    
+    pid_t pid = fork();
+    switch (pid){
+        case -1:
+            perror("Error: pid_t pid == -1. \n");
+            exit(-1);
+        break;
+        case 0: 
+            printf("Hijo: %d - %d\n", getpid(), getppid());
+            close(fd[0]);//importante!!!!!
+            dup2(fd[1],1);
+            close(fd[1]);
+        break;
+        default: 
+            printf("Padre: %d - %d\n", getpid(), pid);
+            close(fd[1]);//importante!!!!!
+            dup2(fd[0],0);
+            close(fd[0]);
+        break;
+    }
+
 
 
     return 0;
