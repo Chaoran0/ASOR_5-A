113cad2acda5fecde31f8e6604f615b44d04aedd
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-12-04 17:52:25 +0100

    paso 1

--- Practica2.4/ejercicio1.c
+++ Practica2.4/ejercicio1.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 
 int main (int argc, char *argv[]){
+    //int pipe(int fd[2]);
+    //./ejercicio1 echo 12345 wc -c: argv[1] argv[2] argv[3] argv[4] => argc = 5
+
+    if(argc < 5){
+        perror("Error: Argumento insuficiente. \n");
+        exit(-1);
+    }
+
     
 
 
