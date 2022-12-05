98305a7f3753f235b4a5f64c6d500f5148f3df6a
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-12-05 18:14:33 +0100

    Update ejercicio2.c

--- Practica2.4/ejercicio2.c
+++ Practica2.4/ejercicio2.c
@@ -26,13 +26,15 @@ int main(int argc, char **argv){
             perror("Error: pid_t pid == -1. \n");
             exit(-1);
         break;
+
         case 0:
             char bufpadre[256];
-            printf("Hijo: %d - %d\n", getpid(), getppid());
+            //printf("Hijo: %d - %d\n", getpid(), getppid());
             close(p_h[1]);
             close(h_p[0]);
+            //write(h_p[1], "l", 1);
             for(int i = 0; i < 10; i++){
-                read(p_h[1], bufpadre, 256);
+                read(p_h[0], bufpadre, 256);
                 printf("[Hijo] Read: %s\n", bufpadre);
                 sleep(1);
                 write(h_p[1], "l", 1);
@@ -42,16 +44,16 @@ int main(int argc, char **argv){
             close(h_p[1]);
         break;
         default:
-            printf("Padre: %d - %d\n", getpid(), pid);
+            //printf("Padre: %d - %d\n", getpid(), pid);
             close(p_h[0]);
             close(h_p[1]);
             char bufhijo[1];
-            read(h_p[0], bufhijo, 1);
             char bufmensaje[256];
-            while(bufhijo[0] == 'q'){
+            while(bufhijo[0] != 'q'){
                 printf("[Padre] Write: ");
                 scanf("%s", bufmensaje);
                 write(p_h[1], bufmensaje, 256);
+                read(h_p[0], bufhijo, 1);
                 while(bufhijo[0] != 'l' && bufhijo[0]!= 'q'){			
 				    read(h_p[0], bufhijo,1);
 			    }
@@ -59,6 +61,7 @@ int main(int argc, char **argv){
             }
             close(p_h[1]);
             close(h_p[0]);
+        break;
 
     }
 
