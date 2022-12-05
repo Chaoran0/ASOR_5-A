3ee0dc140d31c9b0b1c671d5a12ba4390ff358b8
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-12-05 17:53:15 +0100

    Update ejercicio2.c

--- Practica2.4/ejercicio2.c
+++ Practica2.4/ejercicio2.c
@@ -2,15 +2,17 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+
 
 
 int main(int argc, char **argv){
     int p_h[2], h_p[2];
 
-    if(argc != 2){
+    /*if(argc != 5){
         perror("Error: Argumento insuficiente. \n");
         exit(-1);
-    }
+    }*/
 
     if(pipe(p_h) == -1 || pipe(h_p) == -1){
         perror("Error: Función pipe fallida. \n");
@@ -25,34 +27,38 @@ int main(int argc, char **argv){
             exit(-1);
         break;
         case 0:
-            char bufpadre;
+            char bufpadre[256];
             printf("Hijo: %d - %d\n", getpid(), getppid());
             close(p_h[1]);
             close(h_p[0]);
             for(int i = 0; i < 10; i++){
-                ssize_t sizeRead= read(p_h[1], &bufpadre, 256);
-                
+                read(p_h[1], bufpadre, 256);
+                printf("[Hijo] Read: %s\n", bufpadre);
+                sleep(1);
+                write(h_p[1], "l", 1);
             }
+            write(h_p[1], "q", 1);
             close(p_h[0]);
             close(h_p[1]);
-
-
-
         break;
         default:
             printf("Padre: %d - %d\n", getpid(), pid);
             close(p_h[0]);
             close(h_p[1]);
-            char *bufhijo[1];
+            char bufhijo[1];
             read(h_p[0], bufhijo, 1);
             char bufmensaje[256];
             while(bufhijo[0] == 'q'){
-                printf("[Padre] Write: \n");
+                printf("[Padre] Write: ");
                 scanf("%s", bufmensaje);
                 write(p_h[1], bufmensaje, 256);
+                while(bufhijo[0] != 'l' && bufhijo[0]!= 'q'){			
+				    read(h_p[0], bufhijo,1);
+			    }
 
             }
-
+            close(p_h[1]);
+            close(h_p[0]);
 
     }
 
