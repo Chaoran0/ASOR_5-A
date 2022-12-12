9d0e13a472a8e2c0994425d031768f0e628593c6
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-12-12 17:08:01 +0100

    Update ejercicio2.c

--- Practica2.5/ejercicio2.c
+++ Practica2.5/ejercicio2.c
@@ -13,7 +13,7 @@
 
 int main (int argc, char **argv) {
     struct addrinfo hints;
-    if(argc != 3){
+    if(argc != 2){
         printf("Error: argumento insuficiente\n");
         exit(-1);
     }
