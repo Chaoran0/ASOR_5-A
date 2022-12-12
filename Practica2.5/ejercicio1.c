46c6ee2ef4a363d605b5e03902f84cd9ff6beb39
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2022-12-12 10:08:43 +0100

    Update ejercicio1.c

--- Practica2.5/ejercicio1.c
+++ Practica2.5/ejercicio1.c
@@ -1,14 +1,14 @@
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main (int argc, char **argv) {
-	struct addrinfo hints:
-	struct addrinfo *res, *ptr;
+	struct addrinfo hints;
+	struct addrinfo *res, *tmp;
   
   if (argc != 2) {
         printf("Error: argumento insuficiente\n");
@@ -26,13 +26,60 @@ int main (int argc, char **argv) {
 		exit (-1);
 	}
 
-	for(ptr = res; ptr != NULL; ptr = ptr->ai_next) {
+	for(tmp = res; tmp != NULL; tmp = tmp->ai_next) {
 		char host[NI_MAXHOST];
-		getnameinfo(ptr->ai_addr, ptr->ai_addrlen, host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-		printf("Host: %s\nFamily: %d\nSocket: %d\n", host, ptr->ai_family, ptr->ai_socktype);
+		getnameinfo(tmp->ai_addr, tmp->ai_addrlen, host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+		printf("Host: %s\nFamily: %d\nSocket: %d\n", host, tmp->ai_family, tmp->ai_socktype);
 	}
 
 	freeaddrinfo(res);
 
 	return 0;
 }
+
+
+/*
+usuario_local@pto0519:~/Descargas$ ./ejer localhost
+Host: 127.0.0.1
+Family: 2
+Socket: 1
+Host: 127.0.0.1
+Family: 2
+Socket: 2
+Host: 127.0.0.1
+Family: 2
+Socket: 3
+
+usuario_local@pto0519:~/Descargas$ ./ejer ::1
+Host: ::1
+Family: 10
+Socket: 1
+Host: ::1
+Family: 10
+Socket: 2
+Host: ::1
+Family: 10
+Socket: 3
+usuario_local@pto0519:~/Descargas$ ./ejer 1::3::4
+Error: getaddrinfo failed
+usuario_local@pto0519:~/Descargas$ ./ejer www.google.com
+Host: 142.250.200.132
+Family: 2
+Socket: 1
+Host: 142.250.200.132
+Family: 2
+Socket: 2
+Host: 142.250.200.132
+Family: 2
+Socket: 3
+Host: 2a00:1450:4003:801::2004
+Family: 10
+Socket: 1
+Host: 2a00:1450:4003:801::2004
+Family: 10
+Socket: 2
+Host: 2a00:1450:4003:801::2004
+Family: 10
+Socket: 3
+
+*/
