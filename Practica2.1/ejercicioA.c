1a35ebf940975ec7011c583fcfe9a1e93efa90a5
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2023-10-30 10:25:30 +0100

    Update ejercicioA.c

--- Practica2.1/ejercicioA.c
+++ Practica2.1/ejercicioA.c
@@ -1,27 +1,31 @@
-#include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/utsname.h>
-#include <stdlib.h>
-#include <pwd.h>
 
-int main() {
-   // getuid devuelve el identificador de usuario real del proceso actual.
-   // geteuid devuelve el identificador de usuario efectivo del proceso actual.
-  printf("UID: %d\n", getuid());
-  printf("EUID: %d\n", geteuid());
-  
-  //struct passwd *getpwuid(uid_t uid);
-  struct passwd *pwd;
-  pwd = getpwuid(getuid());
-  printf("Username: %s\n", pwd->pw_name);
-  printf("Home directory: %s\n", pwd->pw_dir);
-  printf("User info: %s\n", pwd->pw_gecos);
-  
-  if (geteuid() != getuid())
-	  printf("bit setuid: Activado \n");
-  
-  return 0;
+
+int main(){
+	struct passwd *pwd;
+	pwd = getpwuid(getuid());
+	printf("Nombre de usuario: %s\n", pwd->pw_name);
+	printf("Contraseña: %s\n", pwd->pw_passwd);
+	printf("Identificador de usuario: %d\n", pwd->pw_uid);
+	printf("Identificador de grupo: %d\n", pwd->pw_gid);
+	printf("Directorio 'home': %s\n", pwd->pw_dir);
+	printf("Shell: %s\n", pwd->pw_shell);
+
+	if(pwd->pw_uid != geteuid())
+		printf("Bit setuid activo\n");
+	else 
+		printf("Bit setuid no activo\n");
+
+	if(pwd->pw_gid != getegid())
+		printf("Bit setgid activo\n");
+	else 
+		printf("Bit setgid no activo\n");
+		
+	return 0;
 }
+
