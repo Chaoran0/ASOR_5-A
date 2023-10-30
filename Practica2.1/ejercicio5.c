7e3817e06a6ad339d82f612b327792f5c773466e
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2023-10-30 09:24:28 +0100

    Update ejercicio5.c

--- Practica2.1/ejercicio5.c
+++ Practica2.1/ejercicio5.c
@@ -1,24 +1,59 @@
+#include <sys/utsname.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/utsname.h>
-#include <stdlib.h>
 
 
-int main() {
-   //int uname(struct utsname *buf);
-  struct utsname buf;
-  if(uname(&buf) == 0){
-    printf("Operating system name (e.g., Linux): %s\n", buf.sysname);
-    printf("Name within 'some implementation-defined network': %s\n", buf.nodename);
-    printf("Operating system release (e.g., '2.6.28'): %s\n", buf.release);
-    printf("Operating system version: %s\n", buf.version);
-    printf("Hardware identifier: %s\n", buf.machine);
-  }
-  else 
-    printf("Error: %d, %s\n", errno, strerror(errno));
-   
-   return 0;
+int main(){
+	struct utsname syst;
+	if (uname(&syst) == 0){
+		printf("Operating system name (e.g., 'Linux'): %s\n", syst.sysname);
+		printf("Name within 'some implementation-definednetwork': %s\n", syst.nodename);
+		printf("Operating system release (e.g., '2.6.28'): %s\n", syst.release);
+		printf("Operating system version: %s\n", syst.version);
+		printf("Hardware identifier: %s\n", syst.machine);
+		#ifdef _GNU_SOURCE
+			printf("NIS or YP domain name: %s\n", syst.domainname);
+		#endif
+	}
+	else 
+		printf("Error %d : %s", errno, strerror(errno));
+	
 }
+
+/*
+RETURN:
+Operating system name (e.g., 'Linux'): Linux
+Name within 'some implementation-definednetwork': pto0515
+Operating system release (e.g., '2.6.28'): 5.15.0-39-generic
+Operating system version: #42-Ubuntu SMP Thu Jun 9 23:42:32 UTC 2022
+Hardware identifier: x86_64
+*/
+
+/*
+DESCRIPTION
+       uname()  returns system information in the structure pointed to by buf.
+       The utsname struct is defined in <sys/utsname.h>:
+
+           struct utsname {
+               char sysname[];    /* Operating system name (e.g., "Linux") */
+               char nodename[];   /* Name within "some implementation-defined
+                                     network" */
+               char release[];    /* Operating system release
+                                     (e.g., "2.6.28") */
+               char version[];    /* Operating system version */
+               char machine[];    /* Hardware identifier */
+           #ifdef _GNU_SOURCE
+               char domainname[]; /* NIS or YP domain name */
+           #endif
+           };
+
+       The length of the arrays  in  a  struct  utsname  is  unspecified  (see
+       NOTES); the fields are terminated by a null byte ('\0').
+RETURN VALUE
+       On  success,  zero is returned.  On error, -1 is returned, and errno is
+       set appropriately.
+
+ERRORS
+       EFAULT buf is not valid.
+*/
