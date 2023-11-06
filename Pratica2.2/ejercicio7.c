6bb1723c44f5a595b0d0bd783b38262603aed646
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2023-11-06 10:04:28 +0100

    Update ejercicio7.c

--- Pratica2.2/ejercicio7.c
+++ Pratica2.2/ejercicio7.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]){
     exit(1);
   }
  
-  fd = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 0777);
+  fd = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, mask);
   
 
   printf("Umask: %o\n", mask2);
