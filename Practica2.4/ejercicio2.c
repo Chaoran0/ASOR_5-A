cf6db7229b1d9cd2de213f6f50b7c2f5be3abfd3
Chaoran0 <100696461+Chaoran0@users.noreply.github.com>
2023-12-04 10:16:52 +0100

    Update ejercicio2.c

--- Practica2.4/ejercicio2.c
+++ Practica2.4/ejercicio2.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <stdlib.h>
 
 
 
-int main(int argc, char **argv){
-    int p_h[2], h_p[2];
-
-    /*if(argc != 5){
-        perror("Error: Argumento insuficiente. \n");
+int main (int argc, char **argv){
+    if (argc != 1) {
+        printf("Usage: %s <string>\n", argv[0]);
         exit(-1);
-    }*/
+    }
+
+    int p_h[2], h_p[2];
 
     if(pipe(p_h) == -1 || pipe(h_p) == -1){
         perror("Error: Función pipe fallida. \n");
@@ -23,48 +24,48 @@ int main(int argc, char **argv){
 
     switch(pid){
         case -1:
-            perror("Error: pid_t pid == -1. \n");
+            perror("Error: pid_t pid == -1\n");
             exit(-1);
-        break;
-
+            break;
         case 0:
-            char bufpadre[256];
-            //printf("Hijo: %d - %d\n", getpid(), getppid());
-            close(p_h[1]);
+            //printf("Hijo:%d - %d\n", getpid(), getppid());
+            char buff_message[256];
             close(h_p[0]);
-            //write(h_p[1], "l", 1);
-            for(int i = 0; i < 10; i++){
-                read(p_h[0], bufpadre, 256);
-                printf("[Hijo] Read: %s\n", bufpadre);
+            close(p_h[1]);
+            for(int i = 0; i<10; i++){
+                read(p_h[0], buff_message, 256);
+                printf("[Hijo]Read: %s\n", buff_message);
                 sleep(1);
                 write(h_p[1], "l", 1);
             }
             write(h_p[1], "q", 1);
-            close(p_h[0]);
+
             close(h_p[1]);
-        break;
+            close(p_h[0]);
+            break;
         default:
-            //printf("Padre: %d - %d\n", getpid(), pid);
-            close(p_h[0]);
+            //printf("Padre:%d - %d\n", getpid(), pid);
             close(h_p[1]);
-            char bufhijo[1];
-            char bufmensaje[256];
-            while(bufhijo[0] != 'q'){
-                printf("[Padre] Write: ");
-                scanf("%s", bufmensaje);
-                write(p_h[1], bufmensaje, 256);
-                read(h_p[0], bufhijo, 1);
-                while(bufhijo[0] != 'l' && bufhijo[0]!= 'q'){			
-				    read(h_p[0], bufhijo,1);
-			    }
-
+            close(p_h[0]);
+            char buff_h[1];
+            char buff_p[256];
+            while(buff_h[0] != 'q'){
+                printf("[Padre]Write: ");
+                scanf("%s", buff_p);
+                write(p_h[1], buff_p, 256);
+                read(h_p[0], buff_h, 1);
+                
             }
-            close(p_h[1]);
             close(h_p[0]);
-        break;
-
+            close(p_h[1]);
+            break;
     }
-
-
+        
     return 0;
+
+
+
+
+
+
 }
